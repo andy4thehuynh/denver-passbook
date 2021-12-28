26482605b551790077ebfc81466852324ebec92c
Andy Huynh <andy4thehuynh@gmail.com>
2021-12-27 21:20:53 -0700

    ALH: Successfully imports a component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import 'bulma/css/bulma.min.css';
-import { Button } from 'react-bulma-components';
 
 import './App.css';
+import FoodList from './components/FoodList';
 
 function App() {
   return (
-    <Button color="primary">My Bulma button</Button>
-  );
+    <div class="container is-fluid">
+      <div class="columns">
+        <div class="column is-4 has-background-primary">
+          <FoodList />
+        </div>
+        <div class="column is-8 is-info has-background-warning">Auto</div>
+      </div>
+    </div>
+	);
 }
 
 export default App;
